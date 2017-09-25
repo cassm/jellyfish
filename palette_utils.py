91e82aec1be8e69a8a91568b3603e6c730250721
Cass May <ingeniousmammal@gmail.com>
2017-09-25 16:55:51 +0100

    Add palette indexing funcs

---
+++ palette_utils.py
@@ -0,0 +1,11 @@
+def get_time_offset(elapsed_time, palette_len):
+    time_factor = palette_len/-150
+    return int(elapsed_time*time_factor) % palette_len
+
+def get_space_offset(pixel_index, pixels_per_string, palette_len):
+    palette_offset_per_pixel = (palette_len/10) / pixels_per_string
+    return int((pixel_index % pixels_per_string) * palette_offset_per_pixel) % palette_len
+
+def get_total_offset(elapsed_time, pixel_index, pixels_per_string, palette_len):
+    return (get_time_offset(elapsed_time, palette_len) + get_space_offset(pixel_index, pixels_per_string, palette_len)) % palette_len
+
