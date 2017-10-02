cfb29490e8df5cfbaa24a743b9bd5185ea68d093
Cass May <ingeniousmammal@gmail.com>
2017-10-02 10:24:41 -0700

    Improve sound response on rainbow waves

--- rainbow_waves.py
+++ rainbow_waves.py
@@ -24,15 +24,23 @@ def set_pixels(pixel_buff, elapsed_time, speed_r, speed_g, speed_b, audio_level,
     audio_factor = 1.0
 
     if audio_respond:
-        audio_factor = audio_level + 0.75
+        audio_factor = 1.0
 
     for ii in range(n_pixels):
         pct = (ii / n_pixels)
         # diagonal black stripes
         pct_jittered = (pct * 77) % 37
+
         blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
+
         blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
-        blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
+
+        if audio_respond:
+            blackstripes = color_utils.clamp(blackstripes +
+                blackstripes_offset, 0+audio_level/2, 0.5+audio_level/2)
+        else:
+            blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
+
         # 3 sine waves for r, g, b which are out of sync with each other
         r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
         g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
